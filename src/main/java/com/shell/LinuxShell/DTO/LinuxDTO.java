3ab896e9c67eb80f7d6b78daaba6320cb58f4ec4
harshyadav02 <harshyadav5736@gmail.com>
2024-09-10 13:33:21 +0530

    Java class to intreact with command

---
+++ src/main/java/com/shell/LinuxShell/DTO/LinuxDTO.java
@@ -0,0 +1,14 @@
+package com.shell.LinuxShell.DTO;
+
+public class LinuxDTO {
+    
+    private String command;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+}
