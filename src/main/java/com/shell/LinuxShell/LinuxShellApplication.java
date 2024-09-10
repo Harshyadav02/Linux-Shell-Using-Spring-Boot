aea20c1423e4f28941e07ab10658710dda36b794
harshyadav02 <harshyadav5736@gmail.com>
2024-09-10 13:36:31 +0530

    properties file

---
+++ src/main/java/com/shell/LinuxShell/LinuxShellApplication.java
@@ -0,0 +1,13 @@
+package com.shell.LinuxShell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxShellApplication.class, args);
+	}
+
+}
