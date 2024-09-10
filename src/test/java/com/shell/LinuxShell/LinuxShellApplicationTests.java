b8d14244a1fdf604d7ccc88703b9d673d1e3495e
harshyadav02 <harshyadav5736@gmail.com>
2024-09-10 13:37:45 +0530

    testing related files

---
+++ src/test/java/com/shell/LinuxShell/LinuxShellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shell.LinuxShell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxShellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
